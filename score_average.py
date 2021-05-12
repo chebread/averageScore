321a1964ae07af5ddc1d0e65adf5902b25aa2a36
차한음 <ihaneum@chahan-eum-ui-MacBookAir.local>
2021-05-12 22:36:36 +0900

    version: 1

--- score_average.py
+++ score_average.py
@@ -1,8 +1,33 @@
 print("-- 시험 평균점수 프로그램 --")
 # 시험 과목 입력
-sub = [] # List object
+amount = int(input("과목(0:quit): "))
+if amount == 0:
+    print("-- 종료 --")
+    exit()
 
+# 과목 점수 입력
+for i in range(1, amount+1):
+    print("-- %d 번째 과목 -- "%i)
+    score_all = int(input("문제: "))
+    score_true = int(input("맞춘 문제: "))
+    # 과목 평균점수
+    score_average = (score_all / score_true) * 100
 
-# 시험 과목 당 점수 입력
+all_average = score_average / amount
 
-# 시험 평균점수 / 과목 당 점수 / 등급 (교육청 표준 시험 등급) 출력
+def Rank(result):
+    if result == 100:
+        return "A"
+    elif 90 <= result < 100:
+        return "B"
+    elif 80 <= result < 90:
+        return "C"
+    elif 70 <= result < 80:
+        return "D"
+    else:
+        return "E"
+rank = Rank(all_average)
+print("-- 시험 결과 --")
+print("과목: {}".format(amount))
+print("평균점수: {}".format(all_average))
+print("등급: {}".format(rank))
